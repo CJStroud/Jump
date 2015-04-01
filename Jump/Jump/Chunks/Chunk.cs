b553748deea841007d3e10f7a5b7891a152afbf8
stroudiee <chris.stroud00@gmail.com>
2015-04-01 20:16:45 +0100

    Added Hole Chunks

---
+++ Jump/Jump/Chunks/Chunk.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Jump.Chunks
+{
+    public class Chunk : Sprite
+    {
+        public bool IsCollidable { get; set; }
+
+        public Chunk(string assetName, Vector2 position, int width, int height)
+            : base(assetName, position, width, height)
+        {
+            IsCollidable = true;
+        }
+    }
+}
---
+++ Jump/Jump/HoleChunk.cs
@@ -0,0 +1,15 @@
+﻿using Jump.Chunks;
+using Microsoft.Xna.Framework;
+
+namespace Jump
+{
+    public class HoleChunk : Chunk
+    {
+        public HoleChunk(string assetName, Vector2 position, int width, int height) : base(assetName, position, width, height)
+        {
+            IsCollidable = false;
+        }
+
+
+    }
+}
